945d10239302d087ffe00d5d90fafc2fa831513f
xkhassoukx <43172332+xkhassoukx@users.noreply.github.com>
2018-10-25 12:14:05 +0200

    Add files via upload

---
+++ Switch.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+        [SerializeField]
+        private GameObject objectToDeactivate;
+
+        [SerializeField]
+        private string objectTagName;
+
+        [SerializeField]
+        private string 
+        objectName;
+        [SerializeField]
+        private bool Activate = false;
+        
+      
+         void Awake()
+    {
+        objectToDeactivate.SetActive(false);
+    }
+
+        private void Start()
+        {
+            if (Activate)
+            {
+                objectToDeactivate = gameObject;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if ((collision.gameObject.CompareTag(objectTagName) || collision.gameObject.name == objectName) && !Activate)
+            {
+            objectToDeactivate.SetActive(false);
+            }
+    /*    if (!Activate)
+        {
+            objectToDeactivate.SetActive(true);
+        }
+    */
+    }
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+        if (collider.gameObject.CompareTag(objectTagName) || collider.gameObject.name == objectName)
+          {
+            objectToDeactivate.SetActive(false);
+          }
+        if (!Activate)
+        {
+            objectToDeactivate.SetActive(true);
+        }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+         Activate = !Activate;
+        }
+}
